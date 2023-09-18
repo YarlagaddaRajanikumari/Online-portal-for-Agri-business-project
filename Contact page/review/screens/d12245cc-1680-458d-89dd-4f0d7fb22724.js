c66ae80c0a29ab74c49279e13cba857d29e481c4
YarlagaddaRajanikumari <yarlagadda.rajani283@gmail.com>
2023-09-18 16:51:50 +0530

    First commit

---
+++ Contact page/review/screens/d12245cc-1680-458d-89dd-4f0d7fb22724.js
@@ -0,0 +1,75 @@
+var content='<div class="ui-page" deviceName="web" deviceType="desktop" deviceWidth="1366" deviceHeight="768">\
+    <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devWeb canvas firer commentable non-processed" alignment="left" name="Template 1" width="1366" height="768">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1691915767673.css" />\
+      <div class="freeLayout">\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+    <div id="s-d12245cc-1680-458d-89dd-4f0d7fb22724" class="screen growth-vertical devWeb canvas PORTRAIT firer ie-background commentable non-processed" alignment="left" name="Screen 1" width="1366" height="768">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/screens/d12245cc-1680-458d-89dd-4f0d7fb22724-1691915767673.css" />\
+      <div class="freeLayout">\
+      <div id="s-Paragraph_3" class="richtext autofit firer ie-background commentable non-processed" customid="H2"   datasizewidth="221.0px" datasizeheight="78.0px" dataX="485.0" dataY="127.0" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Paragraph_3_0">CONTACT US<br /><br /></span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Input_1" class="text firer commentable non-processed" customid="Input 1"  datasizewidth="466.1px" datasizeheight="73.2px" dataX="721.1" dataY="233.0" ><div class="backgroundLayer">\
+        <div class="colorLayer"></div>\
+        <div class="imageLayer"></div>\
+      </div><div class="borderLayer"><div class="paddingLayer"><div class="content"><div class="valign"><input type="text"  value="" maxlength="100"  tabindex="-1" placeholder="Name:"/></div></div>  </div></div></div>\
+      <div id="s-Input_2" class="text firer commentable non-processed" customid="Input 2"  datasizewidth="462.3px" datasizeheight="75.8px" dataX="725.0" dataY="342.0" ><div class="backgroundLayer">\
+        <div class="colorLayer"></div>\
+        <div class="imageLayer"></div>\
+      </div><div class="borderLayer"><div class="paddingLayer"><div class="content"><div class="valign"><input type="text"  value="" maxlength="100"  tabindex="-1" placeholder="Email Id:"/></div></div>  </div></div></div>\
+      <div id="s-Input_3" class="text firer commentable non-processed" customid="Input 3"  datasizewidth="466.5px" datasizeheight="131.0px" dataX="728.5" dataY="460.0" ><div class="backgroundLayer">\
+        <div class="colorLayer"></div>\
+        <div class="imageLayer"></div>\
+      </div><div class="borderLayer"><div class="paddingLayer"><div class="content"><div class="valign"><input type="text"  value="" maxlength="100"  tabindex="-1" placeholder="Comment:"/></div></div>  </div></div></div>\
+      <div id="s-Button_1" class="button multiline manualfit firer commentable non-processed" customid="Send"   datasizewidth="150.0px" datasizeheight="45.0px" dataX="1090.0" dataY="649.0" >\
+        <div class="backgroundLayer">\
+          <div class="colorLayer"></div>\
+          <div class="imageLayer"></div>\
+        </div>\
+        <div class="borderLayer">\
+          <div class="paddingLayer">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Button_1_0">Send</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+\
+      <div id="s-Image" class="image firer ie-background commentable non-processed" customid="Image"   datasizewidth="462.5px" datasizeheight="419.5px" dataX="133.0" dataY="205.0"   alt="image">\
+        <div class="borderLayer">\
+        	<div class="imageViewport">\
+        		<img src="./images/b6a7739b-511a-4c2c-bc0c-c6880fa3c3ce.jpeg" />\
+        	</div>\
+        </div>\
+      </div>\
+\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
